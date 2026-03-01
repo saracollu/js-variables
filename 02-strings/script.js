39039b111ea5f0d677b262ee26ba151df4b75574
saracollu <saracollu.ws@gmail.com>
2026-03-01 13:55:00 +0100

    es 1 e 2 script

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -2,10 +2,19 @@
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
 
+let name = 'Sara'
+console.log(name)
+
+
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+
+const number = 3;
+const text = 'gatto Matto';
+const bool = 'true';
+console.log(number + text + bool)
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
